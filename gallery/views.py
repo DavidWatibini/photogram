069efb691b7e0f9cf2858867e37e3cbf081df7d9
Watinguyaz <watimakhanu@gmail.com>
2019-02-28 16:24:58 +0300

    project's MVC structure set and working

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def start(request):
+
+    title =('Home - Intrest your life Today')
+
+    return render(request, 'index.html',)
