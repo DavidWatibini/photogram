04359cc3106d6cb20a5f3203058d33661b6d72f3
Watinguyaz <watimakhanu@gmail.com>
2019-03-04 15:55:15 +0300

    navbar added

--- gallery/urls.py
+++ gallery/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url(r'^$',views.start),
+    url(r'^$',views.start,name = 'newsToday'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
