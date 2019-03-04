c35c67ea51acdc69615d83b9f57279dc9eb8dc45
Watinguyaz <watimakhanu@gmail.com>
2019-03-04 14:35:11 +0300

    img showing

--- gallery/models.py
+++ gallery/models.py
@@ -6,7 +6,7 @@ from django.db import models
 #for category_det
 
 class Category(models.Model):
-    category_det = models.CharField(max_length=40, unique=True)
+    category_det = models.CharField(max_length=50)
 
     def __str__(self):
         return self.category_det
@@ -20,9 +20,9 @@ class Location(models.Model):
         return self.location_det
 
 class Image(models.Model):
-    image_name = models.CharField(max_length =50)
+    image_name = models.CharField(max_length =30,default='random')
     image_description = models.TextField()
-    image_location = models.ImageField(upload_to = 'images/')
+    image_path = models.ImageField(upload_to = 'gallery/')
     image_location = models.ForeignKey(Location)
     image_category = models.ForeignKey(Category)
 
