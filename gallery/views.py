4433186bc43e930a7f71010671466a06c587b256
Watinguyaz <watimakhanu@gmail.com>
2019-03-04 16:48:57 +0300

    search functionanlity set

--- gallery/views.py
+++ gallery/views.py
@@ -9,3 +9,16 @@ def start(request):
 
     all_images = Image.objects.all()
     return render(request,'index.html', {'all_images':all_images})
+
+def search_results(request):
+
+    if 'category' in request.GET and request.GET["category"]:
+        search_term = request.GET.get("category")
+        searched_category = Category.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-news/search.html',{"message":message,"articles": searched_category})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
