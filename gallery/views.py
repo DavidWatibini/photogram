ca75edf94ddc938f2a8de1aa97008ccd60d50cee
Watinguyaz <watimakhanu@gmail.com>
2019-03-05 10:08:13 +0300

    search function working

--- gallery/views.py
+++ gallery/views.py
@@ -7,18 +7,18 @@ from .models import Image,Category,Location
 
 def start(request):
 
-    all_images = Image.objects.all()
-    return render(request,'index.html', {'all_images':all_images})
+    index_images = Image.objects.all()
+    return render(request,'index.html', {'index_images':index_images})
 
 def search_results(request):
 
-    if 'image' in request.GET and request.GET["image"]:
-        search_term = request.GET.get("image")
-        searched_images = Image.search_by_title(search_term)
+    if 'category' in request.GET and request.GET["category"]:
+        search_term = request.GET.get("category")
+        searched_image = Image.search_by_category(search_term)
         message = f"{search_term}"
 
-        return render(request, 'search.html',{"message":message,"images": searched_images})
+        return render(request, 'index.html',{"message":message,"index_images": searched_image})
 
     else:
         message = "You haven't searched for any term"
-        return render(request, 'search.html',{"message":message})
+        return render(request, 'index.html',{"message":message})
